f9f06ab000ae2227ef0e59ae6b3240000e38c572
Intexa <theintexa@gmail.com>
2026-06-17 12:19:16 +0530

    row fix

--- src/components/site/FeaturedProducts.tsx
+++ src/components/site/FeaturedProducts.tsx
@@ -134,7 +134,7 @@ export function FeaturedProducts({ showButtons = true }: { showButtons?: boolean
             </p>
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 max-w-5xl mx-auto">
             {products.map((product, i) => (
               <div
                 key={i}
